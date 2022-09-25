aa61e7ff1348f1b4bc7e094d36f01f21c54b12cb
Gokul <gokul@Gokuls-MacBook-Pro.local>
2022-09-26 04:09:34 +0530

    finished refactoring

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,47 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns always string type", () => {
+    let trivialKey = deterministicPartitionKey();
+    expect(typeof trivialKey).toBe('string')
+    
+    trivialKey = deterministicPartitionKey(1234);
+    expect(typeof trivialKey).toBe('string')
+
+    trivialKey = deterministicPartitionKey({});
+    expect(typeof trivialKey).toBe('string')
+
+    trivialKey = deterministicPartitionKey(new Date());
+    expect(typeof trivialKey).toBe('string')
+    trivialKey = deterministicPartitionKey(undefined);
+    expect(typeof trivialKey).toBe('string')
+  });
+
+  it("Returns always string with leenth 128 exept no input given", () => {
+    let trivialKey = deterministicPartitionKey();
+    expect(trivialKey.length).toBe(1);
+    
+    trivialKey = deterministicPartitionKey(1234);
+    expect(trivialKey.length).toBe(128);
+    
+
+    trivialKey = deterministicPartitionKey({});
+    expect(trivialKey.length).toBe(128)
+    
+
+    trivialKey = deterministicPartitionKey(new Date());
+    expect(trivialKey.length).toBe(128)
+    
+  });
+
+  it("It should return same key for same input", () => {
+    const val = 'abc'
+    const trivialKey = deterministicPartitionKey(val);
+    const newTrivialKey = deterministicPartitionKey(val);
+    expect(trivialKey).toMatch(newTrivialKey);
+  });
+
+
+
 });
